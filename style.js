32c31858eaae1dec68b57a56c8e069cd87ce52e2
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-10-10 17:03:09 +0200

    footer

--- style.js
+++ style.js
@@ -1,1 +1,12 @@
 
+$(function() {
+    var gallery = $('.media');
+    gallery.on('mouseover', 'img', function() {
+      $(this).css('filter', 'grayscale(0%)');
+      $(this).css('-webkit-filter', 'grayscale(0%)');
+    });
+    gallery.on('mouseout', 'img', function() {
+      $(this).css('filter', 'grayscale(100%)');
+      $(this).css('-webkit-filter', 'grayscale(100%)');
+    });
+  });
